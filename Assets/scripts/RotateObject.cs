ca60dfb238bd6a34e0a4677b3877ad86127175eb
JF_R <robin.jeanfrancois@gmail.com>
2018-06-07 10:48:53 +0200

    initial commit

---
+++ Assets/scripts/RotateObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+
+	public float speed;
+    public bool direction = false;
+    private float myFactor = 1;
+
+    // Update is called once per frame
+
+
+    //public override void Update () {
+    void Update() {
+
+        if (direction == true) { myFactor = -1; }
+        else { myFactor = 1; }
+
+        // ...also rotate around the World's Y axis
+        transform.Rotate(0, speed*Time.deltaTime*myFactor, 0, Space.World);
+
+	}
+}
